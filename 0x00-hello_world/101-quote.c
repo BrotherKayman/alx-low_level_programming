eedf789265948bbc24ebf9048745ca760e4078a6
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-14 01:24:21 +0200

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 FILE *errorFile = stderr;
- fprintf(errorFile,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(errorFile,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
