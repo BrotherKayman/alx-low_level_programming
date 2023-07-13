4361dd5deba5e26d3d0a358d0810fbf39680bc7d
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-14 00:47:23 +0200

    Print data type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -  This program prints the size of various types on the computer it is compiled and run on
+ * main - This program prints the size of various types on the computer it is compiled and run on
  * Return: 0 on success
  */
 int main(void)
