434479c5b336a2dce6567c2cc8d1062c882d4032
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-13 23:24:35 +0200

    Print with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
 *Main - Program that prints using puts function
 *return: 0 (successful)
 */
-
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return 0;
 }  
