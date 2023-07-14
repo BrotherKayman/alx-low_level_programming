517c8c5547d76c8ede2bfe1be448ea5882b051c1
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-14 10:31:54 +0200

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Program prints an error
  * Return: 1 on success
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+FILE *errorFile = stderr;
+fprintf(errorFile, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
 }
