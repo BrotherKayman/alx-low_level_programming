589b44962cb9135ab7ecb4e3fddcf08d936624da
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-19 01:32:26 +0200

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+  alph++;
+{
+  int alph = 97;
+  while (alph <= 122)
+    {
+    print_alphabet();
+    return (0);
+}
