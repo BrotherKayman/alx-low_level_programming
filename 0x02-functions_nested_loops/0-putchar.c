cb40fcb8de9a3029d5bff1418fd9f68a13683240
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-19 00:43:55 +0200

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
+#include "main.h"
 /**
  * main - Print using putchar
  * Return: 0 on success
  */
- int main(void)
- {
-     char a = '_putchar';
-putchar(a);
-putchar('\n');
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
- }
+}
