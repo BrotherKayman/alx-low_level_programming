a19fbeb66958eeb5cf3196ccd4fe2f6f20f44d8d
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-20 10:44:10 +0200

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * print_fibanocci - Prints first 50 Fibanacci numbers starting with 1 and 2
- * @n: integer to be printed
+ * print_fibonacci - Prints first 50 Fibanacci numbers starting with 1 and 2
+ * @n: Integer to be printed
  * Return: 0 on success
  */
-
-void print_fibonacci(int n) {
-int fib[50];
+void print_fibonacci(int n)
+{
+long int fib[50];
 int i;
 fib[0] = 1;
 fib[1] = 2;
@@ -14,16 +14,22 @@ for (i = 2; i < n; i++)
 {
 fib[i] = fib[i - 1] + fib[i - 2];
 }
-for (i = 0; i < n; i++) {
-printf("%d", fib[i]);
-if (i < n - 1) {
+for (i = 0; i < n; i++)
+{
+printf("%ld", fib[i]);
+if (i < n - 1)
+{
 printf(", ");
 }
 }
 printf("\n");
 }
-int main()
+/**
+ * main - Entry point of the program
+ * Return: 0 on success
+ */
+int main(void)
 {
 print_fibonacci(50);
-return 0;
+return (0);
 }
