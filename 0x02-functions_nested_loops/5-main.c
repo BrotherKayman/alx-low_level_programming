a647e3cd37dc314341e3b0637880984744b82fb1
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-20 03:47:28 +0200

    print sign

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,35 +1,6 @@
-#include "main.h"
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
+#include <unistd.h>
 int main(void)
 {
-  int r;
-
-  r = print_sign(98);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0xff);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(-1);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-
-  int print_sign();
+  int print_sign(int n);
   return (0);
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,6 @@
  * print_sign - Prints the sign of a number.
  * @n: check number.
  * Return: None.
- * main - Entry Point.
  */
 int print_sign(int n)
 {
@@ -19,8 +18,4 @@ return (0);
 }
 return (0);
 }
-int main(void)
-{
-int print_sign(int n);
-return (0);
-}
+
