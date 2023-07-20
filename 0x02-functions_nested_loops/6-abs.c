63946ed8ce2f619061df2f3fdcaf2c5f3f09d5d4
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-20 04:20:47 +0200

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _abs - Computes the absolute value of the integer argument
- *
+ * @r: Checks the integer
  * Return: value of integer
  */
 int _abs(int r)
 {
 if (r >= 0)
-return(r);
+return (r);
 else
-return(-r);
+return (-r);
 }
