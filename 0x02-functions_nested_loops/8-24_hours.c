293e9f6d1c3fc4aa1e10d7061fc8cefcaa22661a
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-20 06:06:36 +0200

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _jack_bauer - Prints time
+ * Return: 0 Always
+ */
+void _jack_bauer(void)
+{
+int hour, minute;
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
+}
+}
+
