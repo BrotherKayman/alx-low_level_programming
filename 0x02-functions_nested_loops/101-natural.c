1b19894012e3cf5240e2e924db4765bde9ff9389
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-20 11:45:09 +0200

    Natuaral numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * Prints the sum of all the multiples of 3 or 5 below 1024 (excluded)
+ * Generates and prints the sum of all the multiples of 3 or 5 below 1024
  * Return: 0 on Success;
  */
 int main(void)
