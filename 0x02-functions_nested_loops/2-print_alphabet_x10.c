363962583a71a141d1d61b7d498e0e4612328f65
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-20 02:06:55 +0200

    Print alphabets x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,18 @@
 #include "main.h"
 /**
- * main - Program prints alphabets x10
+ * print_alphabet_x10 - Prints alphabets x10
  * Return: 0 Always
  */
-int main(void)
-{
-  print_alphabet_x10();
-  return 0;
-}
 void print_alphabet_x10(void)
 {
-  int x;
-  char alph;
-  for (x = 0; x < 10; x++)
-    {
-  for (alph = 'a'; alph <= 'z'; alph++)
-    {
-      _putchar(alph);
-    }
-  _putchar('\n');
-    }
-
+int x;
+char alph;
+for (x = 0; x < 10; x++)
+{
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+_putchar(alph);
+}
+_putchar('\n');
+}
 }
