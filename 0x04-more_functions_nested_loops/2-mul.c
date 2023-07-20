45661532fd8911cfd04b259eee970f9e5bf703a5
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-20 19:19:51 +0200

    Multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - Function multiplies two digits
+ * Return: multiplied integers
+ * @a: Integer value
+ * @b: Integer value
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int main(void)
+{
+
+  _putchar('S');
+  _putchar('u');
+  _putchar('c');
+  _putchar('c');
+  _putchar('e');
+  _putchar('s');
+  _putchar('s');
+  _putchar('!');
+  _putchar('!');
+  _putchar('\n');
+  return (0);
+}
