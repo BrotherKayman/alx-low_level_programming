e503f1ec4b451b436fa6c697bc78f9a5474781d7
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-20 02:48:46 +0200

    is alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 #include "main.h"
 /**
- *
+ * _isalpha - checks alphabet
+ * @c: checks alphabet character
  *
  * Return: 1 if alphabet, otherwise 0 always
  *
  */
 int _isalpha(int c)
 {
-  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-      return (1);
-  else
-    return (0);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
 }
