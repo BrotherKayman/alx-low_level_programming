73d5d627f4d090f17203e7c42532b208ea55e161
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-21 02:11:26 +0200

    Check if uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+/**
+ * _isupper - checks is inputs are uppercase letters
+ * @c: checks character
+ * Return: 1 if is uppercase, otherwise 0
+ */
 int _isupper(int c)
 {
-if ((c >= 'A') || (c <= 'Z'))
+if ((c >= 'A') && (c <= 'Z'))
 {
-return 1;
+return (1);
 }
-return 0;
+return (0);
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - Function prints the numbers, from 0 to 9
+ * Return: increment
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i >= 9; i++)
+{
+if ((i == 2) || (i == 4))
+continue;
+}
+_putchar(i);
+_putchar('\n');
+}
