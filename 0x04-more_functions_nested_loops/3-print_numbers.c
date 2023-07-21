6e4756f24e1f6195359f01436398663e5e41b267
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-21 02:42:48 +0200

    Print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,11 +3,10 @@
  * print_numbers - Function prints numbers from 0 - 9
  * Return: increment
  */
-
 void print_numbers(void)
 {
 int i;
 for (i = 0; i <= 9; i++)
-_putchar(i);
+_putchar(i + '0');
 _putchar('\n');
 }
