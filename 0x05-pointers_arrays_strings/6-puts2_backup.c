6d167cf95adc3d38cc3e2e81723d8ae17e07ea9a
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-24 18:02:35 +0200

    Print every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2_backup.c
+++ 0x05-pointers_arrays_strings/6-puts2_backup.c
@@ -11,5 +11,9 @@ while (str[i] != '\0' && str[i + 1] != '\0')
 _putchar(str[i]);
 i += 2;
 }
-_putchar('\n');
-}
+ if (str[i] != '\0')
+   {
+     _putchar(str[i]);
+   }
+     _putchar('\n');
+   }
