0b7380da0d3893620f899cf4b80aee0a9e496bae
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-24 15:41:19 +0200

    Swap integer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* swap_int - abc
+* @a: Value of integer
+* @b: Value of Integer
+*/
+void swap_int(int *a, int *b)
+{
+int x = *a;
+*a = *b;
+*b = x;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap_backup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - abc
+ * @a: abc
+ * @b: abc
+ */
+void swap_int(int *a, int *b)
+{
+  int x = *a;
+  *a = *b;
+*b = x;
+}
