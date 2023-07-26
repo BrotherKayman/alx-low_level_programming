6c5f2f043409d29b998e5c7b9cc6fbd2cfc28f6a
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-26 18:11:47 +0200

    Copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy_backup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strncpy - Function copies a string
+ * @dest: Pointer to Character
+ * @src: Pointer to Character
+ * @n: Size of byte
+ * Return: dest 
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+size_t i;
+for (i = 0; i < (size_t)n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < (size_t)n; i++)
+dest[i] = '\0';
+return (dest);
+}
