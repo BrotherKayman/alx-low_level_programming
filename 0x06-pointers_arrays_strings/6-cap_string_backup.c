66ab27d5a525946f85c3c35538390bdb5ebe2940
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-27 16:00:58 +0200

    Capitalize all

--- 0x06-pointers_arrays_strings/6-cap_string_backup.c
+++ 0x06-pointers_arrays_strings/6-cap_string_backup.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <ctype.h>
+#include <stdio.h>
 /**
 * cap_string - Function changes string to uppercase.
 * @str: Pointer to string
@@ -7,8 +8,23 @@
 */
 char *cap_string(char *str)
 {
-int cap_next;
-int i;
+int cap_next = 1;
+int i, j;
+for (i = 0, j = 0; str[i] != '\0'; i++)
+{
+if (!(isspace(str[i]) || ispunct(str[i])))
+{
+str[j++] = str[i];
+}
+else if (cap_next)
+{
+str[j++] = ' ';
+cap_next = 0;
+}
+if (isspace(str[i]) || ispunct(str[i]))
+cap_next = 1;
+}
+str[j] = '\0';
 for (i = 0; str[i] != '\0'; i++)
 {
 if (isspace(str[i]) || ispunct(str[i]))
@@ -23,4 +39,5 @@ cap_next = 0;
 }
 }
 return (str);
+printf("\n");
 }
