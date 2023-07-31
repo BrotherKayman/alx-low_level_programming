0de34836224a8c165a595f5cf7865a087f7df6d8
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-07-31 21:04:43 +0200

    Locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr_backup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: String to search
+ * @needle: Substring to find
+ * Return: Pointer to first substring, or NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+  if (*needle == '\0')
+    {
+      return (haystack);
+    }
+
+  while (*haystack)
+    {
+      char *h = haystack;
+      char *n = needle;
+
+      while (*n && *h == *n)
+	{
+	  h++;
+	  n++;
+	}
+
+      if (*n == '\0')
+	{
+	  return (haystack);
+	}
+
+      haystack++;
+    }
+
+  return (NULL);
+}
