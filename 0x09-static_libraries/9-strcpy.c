13badc95caa0ab40ae23ad2a6732362bec9ea531
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-05 00:54:57 +0200

    Static library

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strncpy - Copies string
+* @dest: Copied string
+* @src: String to be copied
+* @n: Count
+* Return: 0 on success
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+return (dest);
+}
+/**
+* main - Entry point
+* Return: 0 on success
+*/
+int main(void)
+{
+char *_strncpy(char *dest, char *src, int n);
+return (0);
+}
