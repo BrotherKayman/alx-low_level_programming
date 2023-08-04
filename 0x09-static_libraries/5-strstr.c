c5ffbce5fffa030e6020583d4c1537e4c2e2b4bc
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-05 01:47:21 +0200

    Static Library

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+/**
+* _strstr - Locate a substring in a string
+* @haystack: Pointer to string to search in
+* @needle: Pointer to substring to search for
+* Return: Pointer to first occurrence of 'needle', or NULL if not found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+return (NULL);
+}
+/**
+* main - Entry Point
+* Return: 0 on success
+*/
+int main(void)
+{
+char *_strstr(char *haystack, char *needle);
+return (0);
+}
