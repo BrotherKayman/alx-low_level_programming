dba5c0a5923209efcc31bc6191831a36c4a5d9ce
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-05 01:19:16 +0200

    Static library

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _memcpy - Copy memory area
+* @dest: Pointer to copied memory
+* @src: Pointer to source memory area
+* @n: Number of bytes to copy
+* Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return (dest);
+}
+/**
+* main - Entry Point
+* Return: 0 on success
+*/
+int main(void)
+{
+char *_memcpy(char *dest, char *src, unsigned int n);
+return (0);
+}
