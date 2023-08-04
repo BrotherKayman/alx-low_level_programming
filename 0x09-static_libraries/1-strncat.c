2642d86f7938f97ada7c279284ce4dd3d1f19789
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-05 00:41:26 +0200

    Static library

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strncat - Concatenates characters
+* @dest: Pointer to concatenated string
+* @src: String to be concatenated
+* @n: number of characters to be concatenated
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+return (0);
+}
+/**
+* main - Entry point
+* Return: 0 on success
+*/
+int main(void)
+{
+int _atoi(char *s);
+return (0);
+}
