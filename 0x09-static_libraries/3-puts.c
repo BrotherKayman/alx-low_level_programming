4e7863974a3db0ff6a908cc39fbb426ce9f31d92
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-05 00:19:47 +0200

    Static Library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _puts - Print a string
+* @s: string
+*/
+void _puts(char *s)
+{
+}
+/**
+* main - Entry Point
+* Return: 0 on Success
+*/
+int main(void)
+{
+void _puts(char *s);
+return (0);
+}
