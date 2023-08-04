5bfbc4b85c39c8579ebbc67fdaeecf2abd27887d
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-05 00:28:31 +0200

    Static library

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _atoi - Converts string to integer
+* @s: String to be converted
+* Return: 0 on success
+*/
+int _atoi(char *s)
+{
+return (0);
+}
+/**
+* main - Entry point
+* Return: 0 on success
+*/
+int main(void)
+{
+int _atoi(char *s);
+return (0);
+}
