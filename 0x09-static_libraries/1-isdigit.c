5e8321ce8f40ffc84be1bfb23e003d58f2097d4f
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-04 23:53:38 +0200

    Static library

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isdigit - Check if input is digit
+* @c: Input
+* Return: 1 if isdigit, otherwise 0
+*/
+int _isdigit(int c)
+{
+return (0);
+}
+/**
+* main - Entry Point
+* Return: 0 on success
+*/
+int main(void)
+{
+return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _abs - Compute the absolute value of an integer
+* @n: The integer value
+*
+* Return: The absolute value of 'n'
+*/
+int _abs(int n)
+{
+return (0);
+}
+/**
+* main - Entry Point
+* Return: 0 on success
+*/
+int main(void)
+{
+int _abs(int n);
+return (0);
+}
