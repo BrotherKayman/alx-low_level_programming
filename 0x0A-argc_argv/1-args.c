54028d73bfe9dfa3e9f7743b2c8965dc5907684d
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-05 16:47:31 +0200

    Count arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- * @argc: The number of arguments
- * @argv: Array of pointers to the arguments
- * Return: Always 0
- */
+* main - Entry point of the program
+* @argc: The number of arguments
+* @argv: Array of pointers to the arguments
+* Return: Always 0
+*/
 int main(int argc)
 {
 printf("Number of arguments: %d\n", argc - 1);
