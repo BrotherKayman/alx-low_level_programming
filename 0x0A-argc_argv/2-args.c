10c6904b1e73715f4efb1883ad5b7645990c697e
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-05 18:09:47 +0200

    Print arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
-
 /**
- * print_args - Recursive function to print arguments
- * @argc: The number of arguments
- * @argv: Array of pointers to the arguments
- * @index: Current index of the argument to print
- */
+* print_args - Recursive function to print arguments
+* @argc: The number of arguments
+* @argv: Array of pointers to the arguments
+* @index: Current index of the argument to print
+*/
 void print_args(int argc, char *argv[], int index)
 {
-  if (index < argc)
-    {
-      printf("%s\n", argv[index]);
-      print_args(argc, argv, index + 1);
-    }
+if (index < argc)
+{
+printf("%s\n", argv[index]);
+print_args(argc, argv, index + 1);
 }
-
+}
 /**
- * main - Entry point of the program
- * @argc: The number of arguments
- * @argv: Array of pointers to the arguments
- * Return: Always 0
- */
+* main - Entry point of the program
+* @argc: The number of arguments
+* @argv: Array of pointers to the arguments
+* Return: Always 0
+*/
 int main(int argc, char *argv[])
 {
-  print_args(argc, argv, 0);
-  return (0);
+print_args(argc, argv, 0);
+return (0);
 }
