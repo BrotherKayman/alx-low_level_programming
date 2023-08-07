9c369a539152eb801afc6b6606187a4c24d0427d
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-07 18:45:06 +0200

    Array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  char *array =(char *) malloc(sizeof(char));
+  char *array = (char *)malloc(size * sizeof(char));
     unsigned int i; 
     {
     if (size == 0)
