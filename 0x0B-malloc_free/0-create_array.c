8cf2a6510000126a22e30990e171ebd32cd26dbc
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-07 18:49:33 +0200

    Array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,8 +1,11 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdlib.h>
 /**
+ * create_array - Creates an array of characters.
+ * @size: The size of the array.
+ * @c: The character to initialize the array with.
  *
+ * Return: Pointer to new array, or NULL if it fails.
  */
 char *create_array(unsigned int size, char c)
 {
