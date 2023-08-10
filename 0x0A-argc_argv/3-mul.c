96927507f8c230d4587a04c6d8769459aa34b152
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-10 11:23:13 +0200

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - Entry Point
-* @argc: The number of arguments
-* @argv: Pointer to the arguments
-* Return: 1 on error, otherwise 0
+* main - Entry point
+* @argc: The number of command line arguments
+* @argv: An array of strings containing the arguments
+*
+* Return: 0 on success, 1 on error
 */
 int main(int argc, char *argv[])
 {
-int i;
 int num1;
 int num2;
 int result;
-num1 = 0;
-num2 = 0;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-for (i = 0; argv[1][i] != '\0'; i++)
-{
-num1 = num1 * 10 + (argv[1][i] - '0');
-}
-for (i = 0; argv[2][i] != '\0'; i++)
-{
-num2 = num2 * 10 + (argv[2][i] - '0');
-}
-result = (num1 *num2);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 *num2;
 printf("%d\n", result);
 return (0);
 }
