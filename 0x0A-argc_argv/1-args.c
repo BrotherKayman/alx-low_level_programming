c721c041d61c32cefc9a9c08046a4a66cce32397
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-10 11:12:41 +0200

    Print number

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,9 @@
  * @argv: Array of pointers to the arguments
  * Return: Always 0
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
+(void)argv;
 printf("%d\n", argc - 1);
 return (0);
 }
-
