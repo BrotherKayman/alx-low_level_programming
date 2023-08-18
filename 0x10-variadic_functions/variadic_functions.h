c108fd58e0966725486b62c4ab7690b285c24469
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-18 21:38:30 +0200

    Print strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,4 +3,5 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif
