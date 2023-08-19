3128b97f9bd0edd79e3cea9c0331c88933fffe80
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-19 15:32:14 +0200

    variadic_function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_all(const char * const format, ...);
 #endif
