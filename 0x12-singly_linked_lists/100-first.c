1f94b2f92615a234c37393869e0aaea5d107859e
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-26 16:38:05 +0200

    Before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * first - Prints text before main function
+ *
+ */
+void first(char *s)
+{
+    s = "You're beat! and yet, you must allow,\nI bore my house upon my back!";
+    printf("%s\n", s);
+}
