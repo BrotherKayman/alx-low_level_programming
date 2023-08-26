0dd576f510315f4b762aa8c6f5e2d3399532645d
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-08-26 15:02:10 +0200

    Add new node

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 #include <stddef.h>
+#include <stdio.h>
 /**
- * list_len - Counts number of list_t elements
- * @h: Points to the list head
+ * list_len - Counts length of list_t elements
+ * @h: Points to the first node
  * Return: Number of elements in the list
  */
 size_t list_len(const list_t *h)
 {
-  size_t i = 0;
-  const list_t *str = h;
-
-  while (str != NULL)
-    {
-      i++;
-      str = str->next;
-    }
-
-  return (i);
+size_t i = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d]\n", h->len);
+i++;
+h = h->next;
+}
+return (i);
 }
