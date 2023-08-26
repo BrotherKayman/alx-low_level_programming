0cda0daf6dac5733bdecb14f79f91442c4f678ed
Kagiso M <kagisokayman@gmail.com>
2023-08-26 17:02:07 +0200

    Run before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 void first(void)__attribute__ ((constructor));
 /**
- * first - Prints text before main function
- *
- */
-void first()
+* first - Prints text before main function
+*
+*/
+void first(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
