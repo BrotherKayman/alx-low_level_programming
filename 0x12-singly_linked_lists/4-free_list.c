c7c1c1eb255d7fb7136a1d44e4203613cdc105de
Kagiso M <kagisokayman@gmail.com>
2023-08-26 16:05:28 +0200

    betty doc

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,20 +3,17 @@
 #include <string.h>
 #include "lists.h"
 /**
-* free_list -  frees list_t list
+* free_list - frees list_t list
 * @head: First node
 */
 void free_list(list_t *head)
 {
-    list_t *cur_node = head;
-    while (cur_node != NULL)
-    {
-        list_t *next = cur_node->next;
-        free(cur_node->str);
-        free(cur_node);
-        cur_node = next;
-    }
+list_t *cur_node = head;
+while (cur_node != NULL)
+{
+list_t *next = cur_node->next;
+free(cur_node->str);
+free(cur_node);
+cur_node = next;
 }
-
-
-
+}
