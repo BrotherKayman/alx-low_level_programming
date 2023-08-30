0f9e967d4122ceb1c0528136a7fb5c48c12f712c
Kagiso M <kagisokayman@gmail.com>
2023-08-30 07:44:29 +0200

    modify

--- 0x0E-structures_typedef/2-print_dog_backup.c
+++ 0x0E-structures_typedef/2-print_dog_backup.c
@@ -1,10 +1,10 @@
 #include "dog.h"
 #include <stdio.h>
 /**
- * print_dog - Prints details of struct dog.
- * @d: value
- * Return: If D is NULL, print nothing.
- */
+* print_dog - Prints details of struct dog.
+* @d: value
+* Return: If D is NULL, print nothing.
+*/
 void print_dog(struct dog *d)
 {
 if (d == NULL)
@@ -15,4 +15,3 @@ printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
-
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * dog - ...
+ */
 struct dog {
   char *name;
   float age;
