0f5043cebe020b99eee5c52e8ffd0e4e94ca85bd
Kagiso M <kagisokayman@gmail.com>
2023-09-02 11:42:00 +0200

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
 * print_binary - Convert a decimal number to binary and print it
 * @n: Decimal number to be convert and print
@@ -9,17 +10,19 @@
 */
 void print_binary(unsigned long int n)
 {
-int count = sizeof(n) * 8;
+int count = sizeof(n) * 8; /* Number of bits in an unsigned long int */
 int x = count - 1;
 int bit;
-int lead = 0;
+int lead = 0; /* To skip leading zeros */
 while (x >= 0)
 {
-bit = (n >> x) & 1;
+if (n == 0)
+putchar(n);
+bit = (n >> x) & 1; /* Extract the bit at the current index */
 if (bit == 1 || lead)
 {
-printf("%d", bit);
-lead = 1;
+printf("%d", bit); /* Print the extracted bit */
+lead = 1; /* Add a 1 when encountered */
 }
 x--;
 }
