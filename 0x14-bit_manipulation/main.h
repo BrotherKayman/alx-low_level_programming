b833aef2bfa86fa96af8b11e0a6e7283d1c56c2f
Kagiso M <kagisokayman@gmail.com>
2023-09-02 05:24:38 +0200

    Flip bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,8 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 
 #endif
