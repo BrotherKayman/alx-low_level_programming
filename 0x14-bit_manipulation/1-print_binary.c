5f47be8ce90368200c3d1db2b3f7b157bc6dbf63
Kagiso M <kagisokayman@gmail.com>
2023-09-02 11:10:16 +0200

    Remove leading zeros

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_binary - Convert decimal to binary
-* @n: Decimal
+* print_binary - Convert a decimal number to binary and print it
+* @n: Decimal number to be convert and print
+*
+* This function takes a decimal number
+* then prints the binary representation to the standard output
 */
 void print_binary(unsigned long int n)
 {
-int count = sizeof(n);
+int count = sizeof(n) * 8;
 int x = count - 1;
+int bit;
+int lead = 0;
 while (x >= 0)
 {
-int bit = (n >> x) & 1;
+bit = (n >> x) & 1;
+if (bit == 1 || lead)
+{
 printf("%d", bit);
+lead = 1;
+}
 x--;
 }
 }
