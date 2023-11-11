06c7f24dbbf652863b894893d5ea916f50a6a196
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-11 15:07:59 +0200

    count elements

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,6 @@
 /**
 *  dlistint_len - Returns the number of elements in a linked dlistint_t list.
 *  @h: A pointer to the head of the doubly linked list.
-* 
 *  Return: The number of elements in the list.
 */
 size_t dlistint_len(const dlistint_t *h)
