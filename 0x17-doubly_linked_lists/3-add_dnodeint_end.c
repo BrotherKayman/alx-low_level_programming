3a03b28ebb27dc7a530b9ae7011b542f3a32b989
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-11 16:13:17 +0200

    Add node at -1

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,44 +1,31 @@
 #include "lists.h"
-
 /**
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
- * @head: A pointer to a pointer to the head of the doubly linked list.
- * @n: The data to be stored in the new node.
- *
- * Return: The address of the new element, or NULL if it failed.
- */
+* add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+* @head: A pointer to a pointer to the head of the doubly linked list.
+* @n: The data to be stored in the new node.
+*
+* Return: The address of the new element, or NULL if it failed.
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *temp;
-
-    if (head == NULL)
-        return NULL;
-
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
-
-    /* Set the data for the new node */
-    new_node->n = n;
-    new_node->next = NULL;
-
-    /* If the list is empty, set the new node as the head */
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return new_node;
-    }
-
-    /* Find the last node in the list */
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
-
-    /* Link the new node at the end of the list */
-    temp->next = new_node;
-    new_node->prev = temp;
-
-    return new_node;
+dlistint_t *new_node, *temp;
+if (head == NULL)
+return (NULL);
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->next = NULL;
+if (*head == NULL)
+{
+new_node->prev = NULL;
+*head = new_node;
+return (new_node);
+}
+temp = *head;
+while (temp->next != NULL)
+temp = temp->next;
+temp->next = new_node;
+new_node->prev = temp;
+return (new_node);
 }
