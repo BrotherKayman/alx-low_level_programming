2a8fd6a767b085dfa47de6fee1faad52224ac1bb
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-12 22:48:05 +0200

    Get nth node

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,17 +9,15 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
     dlistint_t *new_node, *temp;
-
     if (head == NULL)
         return (NULL);
-
     new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL)
+    {
         return (NULL);
-
     new_node->n = n;
     new_node->next = NULL;
-
+    }
     if (*head == NULL)
     {
         new_node->prev = NULL;
@@ -35,6 +33,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
         temp->next = new_node;
         new_node->prev = temp;
     }
-
     return (new_node);
 }
