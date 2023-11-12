dcbda778740124c4e113a1136e6ea67f09f5759f
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-12 23:33:29 +0200

    Modified

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,33 +1,28 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
- * @head: A pointer to a pointer to the head of the doubly linked list.
- * @n: The data to be stored in the new node.
- *
- * Return: The address of the new element, or NULL if it failed.
- */
+* add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+* @head: A pointer to a pointer to the head of the doubly linked list.
+* @n: The data to be stored in the new node.
+*
+* Return: The address of the new element, or NULL if it failed.
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
-
-    if (head == NULL)
-        return (NULL);
-
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-
-    new_node->n = n;
-    new_node->next = NULL;
-
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-    }
-
-    return new_node; 
+dlistint_t *new_node;
+if (head == NULL)
+return (NULL);
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+new_node->next = NULL;
+if (*head == NULL)
+{
+new_node->prev = NULL;
+*head = new_node;
+}
+return new_node;
 }
