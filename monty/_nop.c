1b055f08768e22b75f1e43aa5c610eb980eac3df
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-18 00:12:21 +0200

    nop function

---
+++ monty/_nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * @brief No operation. Does nothing.
+ *
+ * @param stack Pointer to the stack.
+ * @param line_number Line number where the opcode occurs.
+ */
+void _nop(__attribute__ ((unused)) stack_t **stack,
+		__attribute__ ((unused)) unsigned int line_number)
+{
+	/* No operation */
+}
