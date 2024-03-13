b0ae37ae86be3dab5e11af63d2810df9d1951625
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-03-13 23:24:06 +0200

    update

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 
 /**
- * interpolation_search - Searches for a value in a sorted array of integers
- *                        using the Interpolation search algorithm.
- * @array: Pointer to the first element of the array to search in.
- * @size: Number of elements in the array.
- * @value: Value to search for.
- *
- * Return: The index of the first occurrence of the value in the array.
- *         (-1) if the value is not present in the array or if array is NULL.
- */
+* interpolation_search - Searches for a value in a sorted array of integers
+*                        using the Interpolation search algorithm.
+* @array: Pointer to the first element of the array to search in.
+* @size: Number of elements in the array.
+* @value: Value to search for.
+*
+* Return: The index of the first occurrence of the value in the array.
+*         (-1) if the value is not present in the array or if array is NULL.
+*/
 int interpolation_search(int *array, size_t size, int value)
 {
-    if (array == NULL || size == 0)
-        return (-1);
+int min = 0;
+int max = size - 1;
+int i;
+if (array == NULL || size == 0)
+return (-1);
 
-    int min = 0;
-    int max = size - 1;
+while (min <= max && value >= array[min] && value <= array[max])
+{
+i = min + (((double)(max - min) / (array[max] - array[min]))
+* (value - array[min]));
 
-    while (min <= max && value >= array[min] && value <= array[max])
-    {
-        int i = min + (((double)(max - min) / (array[max] - array[min])) * (value - array[min]));
+printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
 
-        if (array[i] == value)
-            return (i);
+if (array[i] < value)
+min = i + 1;
+else
+max = i - 1;
+}
 
-        if (array[i] < value)
-            min = i + 1;
-        else
-            max = i - 1;
-    }
-
-    printf("Value checked array[%d] is out of range\n", max);
-    return (-1);
+printf("Value checked array[%d] is out of range\n", max);
+return (-1);
 }
