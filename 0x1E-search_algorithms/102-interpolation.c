2a1730ced0fcbe1109fb430cfaacd05664bd1737
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-03-14 15:55:22 +0200

    update

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,7 +1,7 @@
-#include "search_algos"
+#include "search_algos.h"
 /**
-* exponential_search - Searches for a value in a sorted array of integers
-*                      using the Exponential search algorithm.
+* interpolation_search - Searches for a value in a sorted array of integers
+*                         using the Interpolation search algorithm.
 * @array: Pointer to the first element of the array to search in.
 * @size: Number of elements in the array.
 * @value: Value to search for.
@@ -9,22 +9,27 @@
 * Return: The index of the first occurrence of the value in the array.
 *         (-1) if the value is not present in the array or if array is NULL.
 */
-int exponential_search(int *array, size_t size, int value)
+int interpolation_search(int *array, size_t size, int value)
 {
+size_t min = 0;
+size_t max = size - 1;
 if (array == NULL || size == 0)
-return -1;
+return (-1);
 
-if (array[0] == value)
-return 0;
-
-size_t track = 1;
-while (track < size && array[track] < value)
+while (min <= max && value >= array[min] && value <= array[max])
 {
-printf("Value checked array[%zu] = [%d]\n", track, array[track]);
-track *= 2;
+size_t i = min + (((double)(max - min) / (array[max] - array[min]))
+* (value - array[min]));
+
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+if (array[i] == value)
+return (i);
+else if (array[i] < value)
+min = i + 1;
+else
+max = i - 1;
 }
 
-printf("Value found between indexes [%zu] and [%zu]\n", track / 2, track >= size ? size - 1 : track);
-
-return binary_search(array, track / 2, track >= size ? size - 1 : track, value);
+return (-1);
 }
