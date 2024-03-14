cd06433d9168238054a04c58c0e8581f2b185a1c
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-03-14 22:42:37 +0200

    update

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -9,36 +9,24 @@
  *
  * Return: Index where value is located, or -1 if not found
  */
-int advanced_binary_recursive(int *array, int left, int right, int value)
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 {
-int mid;
-int i;
-if (left <= right)
-{
-mid = left + (right - left) / 2;
-printf("Searching in array: ");
-for (i = left; i <= right; i++)
-{
-printf("%d", array[i]);
-if (i < right)
-{
-printf(", ");
-}
-}
-printf("\n");
-if (array[mid] == value)
-{
-if (mid == left || array[mid - 1] != value)
-return (mid);
-else
-return (advanced_binary_recursive(array, left, mid - 1, value));
-}
-if (array[mid] < value)
-return (advanced_binary_recursive(array, mid + 1, right, value));
-else
-return (advanced_binary_recursive(array, left, mid - 1, value));
-}
-return (-1);
+	size_t i;
+
+	if (right < left)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
 }
 /**
  * advanced_binary - Perform advanced binary search in a sorted array
@@ -49,8 +37,9 @@ return (-1);
  * Return: Index where value is located, or -1 if not found
  */
 int advanced_binary(int *array, size_t size, int value)
-{
-if (array == NULL || size == 0)
-return (-1);
-return (advanced_binary_recursive(array, 0, size - 1, value));
-}
+	{
+		if (array == NULL || size == 0)
+			return (-1);
+
+		return (advanced_binary_recursive(array, 0, size - 1, value));
+	}
