5c3251d1240a7da8713de54a391fe5a504dbcc8e
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-03-14 15:45:15 +0200

    update

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,34 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
-
 /**
-* binary_search - Searches for a value in a sorted array of integers
-*                  using the Binary search algorithm.
+* binary_search - Function to search for a value in a sorted array of integers
+* using the Binary search algorithm.
 * @array: Pointer to the first element of the array to search in.
 * @size: Number of elements in the array.
 * @value: Value to search for.
-*
-* Return: The index of the value in the array, or -1 if not found.
+* Return: The index of the first occurrence of the value in the array.
+*         (-1) if the value is not present in the array or array is NULL.
+* Description: Prints the array elements as it compares.
 */
 int binary_search(int *array, size_t size, int value)
 {
-size_t left = 0;
-size_t right = size - 1;
+int min = 0;
+int max = size - 1;
+int mid;
+size_t i;
 
-while (left <= right)
+if (array == NULL)
 {
-size_t mid = left + (right - left) / 2;
-
-if (array[mid] == value)
-return (mid);
-else if (array[mid] < value)
-left = mid + 1;
-else
-right = mid - 1;
+return (-1);
 }
 
+while (min <= max)
+{
+printf("Searching in array: ");
+for (i = min; (int)i <= max; i++)
+{
+if ((int)i < max)
+printf("%d, ", array[i]);
+else
+printf("%d\n", array[i]);
+}
+mid = (min + max) / 2;
+if (array[mid] == value)
+{
+{
+return (mid);
+}
+}
+else if (array[mid] < value)
+min = mid + 1;
+else
+max = mid - 1;
+}
+{
 return (-1);
+}
 }
 
 /**
@@ -47,7 +64,9 @@ size_t bound = 1;
 size_t start, end;
 
 if (array == NULL || size == 0)
+{
 return (-1);
+}
 
 while (bound < size && array[bound] < value)
 {
@@ -60,5 +79,7 @@ end = (bound < size) ? bound : size - 1;
 
 printf("Value found between indexes [%lu] and [%lu]\n", start, end);
 
+{
 return (binary_search(array + start, end - start + 1, value) + start);
+}
 }
