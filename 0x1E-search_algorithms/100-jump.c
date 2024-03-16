e9eef32c9b8a3c15fa5b7b88ca2b5776da18ff02
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-03-16 22:38:56 +0200

    update

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include "search_algos.h"
 /**
 * jump_search - Searches for a value in a sorted array of integers
 *               using the Jump search algorithm.
@@ -11,26 +12,19 @@
 * Return: The index of the first occurrence of the value in the array.
 *         (-1) if the value is not present in the array or if array is NULL.
 */
-#include <stdio.h>
-#include <math.h>
-
 int jump_search(int *array, size_t size, int value)
 {
 int jump, prev, i;
-
 if (array == NULL || size == 0)
 return (-1);
-
 jump = (int)sqrt(size);
 prev = 0;
-
 printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 while (prev + jump < (int)size && array[prev + jump] < value)
 {
 prev += jump;
 printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 }
-
 printf("Value found between indexes [%d] and [%d]\n", prev, prev + jump);
 for (i = prev; i <= prev + jump && i < (int)size; i++)
 {
@@ -38,6 +32,5 @@ printf("Value checked array[%d] = [%d]\n", i, array[i]);
 if (array[i] == value)
 return (i);
 }
-
 return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,43 @@
 #include <stdio.h>
 #include <math.h>
 
+/*structures*/
+/**
+ * struct listint_s - structure for a singly linked list
+ *
+ * @n: an integer value stored in the node
+ * @index: the position of the node in the list
+ * @next: a pointer to the next node in the list
+ *
+ * Description: This structure defines a node for a singly linked list
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Structure for a singly linked list with an express lane
+ *
+ * @n: Integer value stored in the node
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node in the list
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: This structure defines a node for a singly linked list
+ *              with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -14,4 +51,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif /* SEARCH_ALGOS_H */
